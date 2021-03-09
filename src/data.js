de866b3f0db4cc21b138a5049e6f5164bb569aa7
chiyu <654356282@qq.com>
2021-03-09 18:19:15 +0800

    remove all

--- src/data.js
+++
@@ -1,8 +0,0 @@
-const words = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n']
-const defaultData = []
-words.forEach((item, idx) => {
-    defaultData.push({key: idx, data: item + '1', children: []})
-})
-
-
-export default defaultData
